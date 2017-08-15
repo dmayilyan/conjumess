534ca202488a7625e45987d787074ac3677f20d9
Davit Mayilyan <davit.mayilyan@protonmail.ch>
2017-08-16 01:35:25 +0200

    Switched to decoraters, everything is in fire. NW

--- Verben.py
+++ Verben.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from collections import OrderedDict
 
 
 class de_other:
     """German word declination in other languages"""
-
 
     def __init__(self, arg_words):
         super(de_other, self).__init__()
@@ -32,6 +32,7 @@ class de_other:
             print("Non-reflexive verb")
         self.vrb = ""
         self.dec = []  # declination list
+        self.bold = True
 
 
 # reflexive doesn't work
@@ -46,20 +47,43 @@ class de_other:
             # print(self.w_1, self.w_2)
         else:
             self.vrb = self.w_1[:-2]
-            print(self.w_1)
+            # print(self.w_1)
 
+    def colorize(func):
+        def make_red(*args):
+            res = func(*args)
+            return "\033[1;31m{0}\033[0;0m".format(res)
+        return make_red
+
+    @colorize
     def armdec_dict(self):
-        """ making a dictionary for declination """
-        decs_arm = {("ես ", "ում եմ"): "",
-                    ("դու ", "ում ես"): "",
-                    ("նա ", "ում է"): "",
-                    ("մենք ", "ում ենք"): "",
-                    ("դուք/Դուք ", "ում եք"): "",
-                    ("նրանք ", "ում են"): ""}
+        """ making a dictionary for declination \033[1m  \033[0m"""
+        return "Barev"
+        # decs_arm_addons = OrderedDict([(("ես ", "ում եմ"), ""),
+        #                                (("դու ", "ում ես"), ""),
+        #                                (("նա ", "ում է"), ""),
+        #                                (("մենք ", "ում ենք"), ""),
+        #                                (("դուք/Դուք ", "ում եք"), ""),
+        #                                (("նրանք ", "ում են"), "")])
 
-        [print(x[0] + self.vrb + x[1]) for x in decs_arm]
+        # # building the sentence
+        # decs_arm_raw = [x[0] + self.vrb + x[1] for x in decs_arm_addons]
 
-        print(self.vrb)
+        # #  getting the space locations
+        # bold_list = []
+        # for elm in decs_arm_raw:
+        #     space_pos = []
+        #     for i, c in enumerate(elm):
+        #         if " " == c:
+        #             space_pos.append(i)
+
+        #     # making it bold
+        #     bold_list.append(elm[:space_pos[0]] +
+        #                      "\033[1;31m" +
+        #                      elm[space_pos[0]:space_pos[1]] +
+        #                      "\033[0;0m" + elm[space_pos[1]:])
+        #     # print(bold_line)
+        # return bold_list
 
 
 def hy_de(in_verb):
@@ -67,8 +91,13 @@ def hy_de(in_verb):
     # print(in_verb)
     x = de_other(in_verb)
     x.dec_list(in_verb)
-    x.armdec_dict()
-    print(x.dec)
+    # armdec_list = x.armdec_dict()
+    print(x.armdec_dict())
+    # print(x.refl)
+
+    # for i in range(3):
+    #     print(armdec_list[i], armdec_list[i+3], sep="\t")
+
     # if in_verb[-4:-2] == "են":
     #     print("\n\n\n\n")
     #     root = in_verb[2:-4]
